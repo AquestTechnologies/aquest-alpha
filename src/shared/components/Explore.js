b8fab291d082608092e2f959b15d47f61f747dde
dherault <david@aquest.fr>
2015-08-04 14:32:08 +0000

    refactoring

--- src/shared/components/Explore.js
+++ src/shared/components/Explore.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Node  from './explore/Node';
 import { Link } from 'react-router';
 
-
 export default class Explore extends React.Component {
   
   static runPhidippides(routerState) {
@@ -17,39 +16,21 @@ export default class Explore extends React.Component {
     if (Object.keys(this.props.universes).length < 2) this.props.readUniverses();
   }
   
-  renderList(universes) {
-    return Object.keys(universes).sort()
-    .map(key =>
-      <Node 
-        key={key} 
-        universe={universes[key]} 
-        transitionTo={this.props.transitionTo}
-      />
-    );
-  }
-  
   render() {
-    // CSS temporaire
+    const {universes} = this.props;
     const divStyle = {
       width: '60%',
       margin: '0 auto 0 auto',
       fontSize: '2rem',
     };
     
-    const {universes} = this.props;
-    
-    return (
-      <div>
-        <div style={divStyle}>
-          <Link to='/'>Home</Link>
-          {' - '}
-          <Link to='/Create_universe'>Create a new universe</Link>
-          <br/>
-          <br/>
-          { this.renderList(universes) }
-        </div>
-      </div>
-    );
+    return <div style={divStyle}>
+      <Link to='/'>Home</Link>
+      {' - '}
+      <Link to='/Create_universe'>Create a new universe</Link>
+      <br/>
+      <br/>
+      { Object.keys(universes).sort().map(key => <Node key={key} universe={universes[key]} />) }
+    </div>;
   }
-  
 }
